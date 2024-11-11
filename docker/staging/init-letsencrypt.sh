49f5c31eb9a9224e326c8fe88f9a7d6725e98727
Juan Barbosa <js.barbosa10@uniandes.edu.co>
2024-11-11 16:54:11 -0500

    added letsencrypt for status

--- docker/staging/init-letsencrypt.sh
+++ docker/staging/init-letsencrypt.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-domains=(ai4fin.tech)
+domains=(ai4fin.tech, status.ai4fin.tech)
 rsa_key_size=4096
 data_path="./data/certbot"
 email="juan.barbosa@ai4fin.tech" # Adding a valid address is strongly recommended
